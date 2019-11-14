debc33f2978ccae2c7bc95eb820ff80f49e38724
Yalım Yaşaroğlu <yalim.yasaroglu@obss.com.tr>
2019-11-14 10:31:17 +0300

    added cart boilderplate

--- cart/src/main/java/tr/com/aselsan/uhkks/micro/cart/controllers/CartController.java
+++ cart/src/main/java/tr/com/aselsan/uhkks/micro/cart/controllers/CartController.java
@@ -1,8 +1,51 @@
 package tr.com.aselsan.uhkks.micro.cart.controllers;
 
+import java.util.List;
+import java.util.UUID;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
+import tr.com.aselsan.uhkks.micro.cart.entities.Cart;
+import tr.com.aselsan.uhkks.micro.cart.services.CartService;
 
 @RestController
 public class CartController {
+
+  @Autowired
+  private CartService service;
+
+  @PutMapping("{id}")
+  public Cart createCart(@PathVariable("id") UUID productId){
+    return service.createCart(productId);
+  }
+  /*@PostMapping
+  public Cart addProductToCart(UUID productId, UUID cartId){
+    return null;
+  }
+  @PostMapping
+  public Cart removeProductFromCart(UUID productId, UUID cartId){
+    return null;
+  }
+  @PutMapping
+  public boolean buy(UUID id){
+    return false;
+  }*/
+
+  @GetMapping
+  public List<Cart> getCarts(){
+    return service.getAllCarts();
+  }
+  @GetMapping("{id}")
+  public Cart getCartByID(@PathVariable("id") UUID id){
+    return service.getCartById(id);
+  }
+  @DeleteMapping("{id}")
+  public boolean deleteCartById( @PathVariable("id")UUID id){
+    return service.deleteCartById(id);
+  }
 
 }
--- cart/src/main/java/tr/com/aselsan/uhkks/micro/cart/services/CartService.java
+++ cart/src/main/java/tr/com/aselsan/uhkks/micro/cart/services/CartService.java
@@ -1,8 +1,37 @@
 package tr.com.aselsan.uhkks.micro.cart.services;
 
+import java.util.List;
+import java.util.UUID;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import tr.com.aselsan.uhkks.micro.cart.entities.Cart;
+import tr.com.aselsan.uhkks.micro.cart.repositories.CartRepository;
 
 @Service
 public class CartService {
+
+  @Autowired
+  CartRepository repository;
+
+  public List<Cart> getAllCarts(){
+    return repository.findAll();
+  }
+
+  public Cart getCartById(UUID id){
+    return repository.getOne(id);
+  }
+
+  public boolean deleteCartById(UUID id){
+    repository.deleteById(id);
+    return true;
+  }
+
+  public Cart createCart(UUID productId){
+    Cart cart= new Cart();
+    cart.getProductIds().add(productId);
+    cart.setId(UUID.randomUUID());
+    return repository.save(cart);
+  }
+
 
 }
